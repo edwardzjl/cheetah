f4d83f80a3dc6556229d48aaafe8af398ec6ec5b
david <jiangdw@zju.edu.cn>
2016-06-02 12:54:48 +0800

    Add streaming loader & sandbox modules

---
+++ cheetah-streaming/src/main/java/cn/edu/zju/cheetah/streaming/App.java
@@ -0,0 +1,13 @@
+package cn.edu.zju.cheetah.streaming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
