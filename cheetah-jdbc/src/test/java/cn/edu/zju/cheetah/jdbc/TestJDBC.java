cc3ecad8a820bfd7c1997da7339451d40a58d91a
Jiarong Wei <jiarong13@163.com>
2016-06-26 20:58:27 +0800

    Update testCheetahJDBC()

--- cheetah-jdbc/src/test/java/cn/edu/zju/cheetah/jdbc/TestJDBC.java
+++ cheetah-jdbc/src/test/java/cn/edu/zju/cheetah/jdbc/TestJDBC.java
@@ -4,7 +4,9 @@
 package cn.edu.zju.cheetah.jdbc;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
 
@@ -18,26 +20,49 @@ public class TestJDBC extends TestCase {
 
   public void testCheetahJDBC() throws Exception {
     String dbUrl = "jdbc:cheetah:http:@default";
-    String sql = "select * from table";
+    String tableName = "metrics";
+    String sql = "SELECT * FROM " + tableName
+        + " WHERE interval BETWEEN '2016-01-01' AND '2017-01-01' LIMIT 100";
     Class.forName(CheetahDriver.class.getName());
-    
+
     Properties cheetah = new Properties();
-    cheetah.setProperty(CheetahCluster.BROKER_HOST, "localhost");
-    cheetah.setProperty(CheetahCluster.BROKER_PORT, "8023");
-    cheetah.setProperty(CheetahCluster.COORDINATOR_HOST, "localhost");
-    cheetah.setProperty(CheetahCluster.COORDINATOR_PORT, "9078");
-    cheetah.setProperty(CheetahCluster.OVERLORD_HOST, "localhost");
-    cheetah.setProperty(CheetahCluster.OVERLORD_HOST, "7856");
-    
+    cheetah.setProperty(CheetahCluster.BROKER_HOST, "10.214.208.59");
+    cheetah.setProperty(CheetahCluster.BROKER_PORT, "8082");
+    cheetah.setProperty(CheetahCluster.COORDINATOR_HOST, "10.214.208.59");
+    cheetah.setProperty(CheetahCluster.COORDINATOR_PORT, "8081");
+    cheetah.setProperty(CheetahCluster.OVERLORD_HOST, "10.214.208.59");
+    cheetah.setProperty(CheetahCluster.OVERLORD_PORT, "8090");
+
     Connection dbConn = DriverManager.getConnection(dbUrl, cheetah);
-//    DatabaseMetaData dbmd = dbConn.getMetaData();
-//    dbmd.getTables(catalog, schemaPattern, tableNamePattern, types);
-//    dbmd.getColumns(catalog, schemaPattern, tableNamePattern, columnNamePattern);
-    Statement stmt = dbConn.createStatement();
-    stmt.executeQuery(sql);
     System.out.println(dbConn);
-    System.out.println(stmt);
     System.out.println(dbConn.getCatalog());
+
+    DatabaseMetaData dbmd = dbConn.getMetaData();
+    ResultSet rs;
+    rs = dbmd.getTables(null, null, null, null);
+    System.out.println("Tables: ");
+    while (rs.next()) {
+      System.out.println(rs.getString(1));
+    }
+    rs.close();
+    rs = dbmd.getColumns(null, null, tableName, null);
+    System.out.println("Columns of table [" + tableName + "]: ");
+    while (rs.next()) {
+      System.out.println(rs.getString(2));
+    }
+    rs.close();
+
+    Statement stmt = dbConn.createStatement();
+    System.out.println(stmt);
+    rs = stmt.executeQuery(sql);
+    System.out.println("Result of SQL \"" + sql + "\": " + rs.getRow());
+    while (rs.next()) {
+      for (int i = 1; i <= 11; i++) {
+        System.out.print(rs.getString(i) + " | ");
+      }
+      System.out.println();
+    }
+    rs.close();
   }
 
 }
