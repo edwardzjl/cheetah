34ef0966684e9b32c7ce43d65410fe2d64bb70d4
Jiarong Wei <jiarong13@163.com>
2016-06-29 10:49:53 +0800

    Retrieve columns type in a crude way

--- cheetah-jdbc/src/main/java/cn/edu/zju/cheetah/jdbc/CheetahDatabaseMetaData.java
+++ cheetah-jdbc/src/main/java/cn/edu/zju/cheetah/jdbc/CheetahDatabaseMetaData.java
@@ -10,9 +10,16 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.RowIdLifetime;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.yahoo.sql4d.sql4ddriver.BrokerAccessor;
 import com.yahoo.sql4d.sql4ddriver.DDataSource;
+import com.yahoo.sql4d.sql4ddriver.Mapper4All;
 
 import scala.Tuple2;
 import scala.util.Either;
@@ -1101,6 +1108,52 @@ public class CheetahDatabaseMetaData implements DatabaseMetaData {
     // TODO Auto-generated method stub
     return null;
   }
+  
+  private Map<String, String> retrieveColumnType(String tableName, List<String> columns) {
+    BrokerAccessor accessor = new BrokerAccessor("10.214.208.59", 8082, 10000);
+    String json = "{\"queryType\":\"segmentMetadata\",\"dataSource\":\""+ tableName +"\"}";
+    Either<String, Either<Mapper4All, JSONArray>> queryResult = accessor.fireQuery(json, null, false);
+    JSONArray jsonArray = queryResult.right().get().right().get();
+
+    JSONObject columnsInfo = jsonArray.getJSONObject(0).getJSONObject("columns");
+    Map<String, String> columnsType = new HashMap<>();
+    for (String column : columns) {
+      String type = columnsInfo.getJSONObject(column).getString("type");
+      System.out.println(column + ": " + type);
+      columnsType.put(column, type);
+    }
+    return columnsType;
+  }
+
+  private static int toSqlType(String type) {
+    switch (type) {
+      case "STRING":
+        return java.sql.Types.VARCHAR;
+      case "LONG":
+        return java.sql.Types.INTEGER;
+      case "FLOAT":
+        return java.sql.Types.FLOAT;
+      case "DOUBLE":
+        return java.sql.Types.DOUBLE;
+      default:
+        return java.sql.Types.VARCHAR;
+    }
+  }
+
+  private static String toSqlTypeName(String type) {
+    switch (type) {
+      case "STRING":
+        return "VARCHAR";
+      case "LONG":
+        return "INTEGER";
+      case "FLOAT":
+        return "FLOAT";
+      case "DOUBLE":
+        return "DOUBLE";
+      default:
+        return "VARCHAR";
+    }
+  }
 
   @Override
   public ResultSet getColumns(String catalog, String schemaPattern, String tableNamePattern,
@@ -1110,14 +1163,28 @@ public class CheetahDatabaseMetaData implements DatabaseMetaData {
     if (dataSourceDescRes.isLeft())
       throw new SQLException(dataSourceDescRes.left().get());
 
-    List<String> dims = dataSourceDescRes.right().get()._1();
     TableSchema schema = new TableSchema();
     schema.addColumn(new ColumnSchema("TABLE_NAME", java.sql.Types.VARCHAR));
     schema.addColumn(new ColumnSchema("COLUMN_NAME", java.sql.Types.VARCHAR));
+    schema.addColumn(new ColumnSchema("DATA_TYPE", java.sql.Types.VARCHAR));
+    schema.addColumn(new ColumnSchema("TYPE_NAME", java.sql.Types.VARCHAR));
+
     InMemTable memTable = new InMemTable(schema);
+
+    List<String> dims = dataSourceDescRes.right().get()._1();
+    Map<String, String> dimsType = retrieveColumnType(tableNamePattern, dims);
     for (String dim : dims) {
-      memTable.append(Tuple.of(tableNamePattern, dim));
+      memTable.append(Tuple.of(tableNamePattern,
+              dim, toSqlType(dimsType.get(dim)), toSqlTypeName(dimsType.get(dim))));
     }
+
+    List<String> metrics = dataSourceDescRes.right().get()._2();
+    Map<String, String> metricsType = retrieveColumnType(tableNamePattern, metrics);
+    for (String metric : metrics) {
+      memTable.append(Tuple.of(tableNamePattern,
+              metric, toSqlType(metricsType.get(metric)), toSqlTypeName(metricsType.get(metric))));
+    }
+
     return new CheetahResultSet(memTable);
   }
 
